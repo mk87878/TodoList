3d4fb2bf0ab96f22cb9dc6749aacddf8338096e6
mingwen <mk87878@163.com>
2015-12-08 18:49:44 +0800

    完成输入一句话创建任务

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 /**
  * Created by zhangmingwen on 15/12/8.
  */
+angular
+    .module('todoApp',[])//创建程序模块
+    .controller('toDoCtrl',['$scope',function($scope){
+        $scope.todoList = [];
+        $scope.add = function(){
+            $scope.todoList.push($scope.content);//获取提交内容
+            $scope.content = '';//清空form
+        };
+    }]);
