9ad321aab34515318f4ad03e22ca93827018ad84
mingwen <mk87878@163.com>
2015-12-08 22:20:15 +0800

    记录创建任务时间，未完成3：结束时间

--- js/app.js
+++ js/app.js
@@ -4,12 +4,27 @@
 angular
     .module('todoApp',[])//创建程序模块
     .controller('toDoCtrl',['$scope',function($scope){
+
         $scope.todoList = [];
+        $scope.teamList = [
+            {team:0},
+        ];
+
         $scope.add = function(){
             $scope.todoList.push({
-                content:$scope.content,//获取提交文字内容
+                content:$scope.content,//获取提交任务内容
                 color:$scope.color,//获取提交的css样式名称
+                team:0,//分组
+                date:getNowDate(),
             });
-            $scope.content = '';//清空form
+            $scope.content = '';//清空内容
         };
+
+
+
+
     }]);
+
+function getNowDate(){//获取当前系统时间
+    return new Date().toLocaleDateString()
+}
