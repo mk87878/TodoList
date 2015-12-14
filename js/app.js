eb82a0d37bcaa9dc92532050b14f547d9bc71583
mingwen <mk87878@163.com>
2015-12-14 15:41:41 +0800

    初步整改app.js文件，整体布局定义完成

--- js/app.js
+++ js/app.js
@@ -1,8 +1,34 @@
 /**
  * Created by zhangmingwen on 15/12/8.
  */
+(function(angular, undefined) {'use strict';
 angular
     .module('todoApp',[])//创建程序模块
+    .value('todoList',[])
+    .controller['addCtrl',addCtrl]
+    .controller['listCtrl',listCtrl]
+    .service('taskCtrl',tasksCtrl)
+;
+function addCtrl($scope, todoList){
+    $scope.add =add;
+    function add(){
+        $scope.todoList.push({
+            content:$scope.content,//获取提交任务内容
+            color:$scope.color,//获取提交的css样式名称
+            team:0,//分组
+            date:getNowDate(),
+        });
+        $scope.content = '';//清空内容
+    };
+}
+function listCtrl($scope){}
+function tasksCtrl($scope){}
+
+
+
+
+
+
     .controller('toDoCtrl',['$scope',function($scope){
 
         $scope.todoList = [];
@@ -33,3 +59,6 @@ angular
 function getNowDate(){//获取当前系统时间
     return new Date().toLocaleDateString()
 }
+
+
+})(window.angular);
