bbbff402bb796e391c03dbeb654f0ecaeca1cf4d
mingwen <mk87878@163.com>
2015-12-16 17:39:56 +0800

    完成本地存储，删除任务，勾选任务。分状态分组显示（已完成和未完成）

--- js/app.js
+++ js/app.js
@@ -2,17 +2,16 @@
  * Created by zhangmingwen on 15/12/8.
  */
 angular
-    .module('todoApp',['ngStorage'])//创建程序模块
-    .controller('toDoCtrl',['$scope',function(
-        $scope
-        //$localStorage,
-        //$sessionStorage,
-    ){
-        $scope.todoList = [];//存储所以todo
+    .module('todoApp',[])//创建程序模块
+    .controller('toDoCtrl',['$scope',function($scope){
+        var temp = localStorage.getItem('todoList');
 
+        $scope.todoList = temp ? angular.fromJson(temp) : [];//存储所有todo
 
+        //$scope.todoList = [];//存储所以todo
 
-        $scope.colorOptions = [//todo列表颜色
+        //todo列表颜色
+        $scope.colorOptions = [
             {name:'green',code:'list-group-item-success'},
             {name:'red',code:'list-group-item-danger'},
             {name:'blue',code:'list-group-item-info'},
@@ -24,13 +23,15 @@ angular
         //用不起:原因未知
         */
 
-        $scope.createTodo = {//初始化颜色
+        //初始化
+        $scope.createTodo = {
             content:'',//任务内容
             color:'list-group-item-info',//css样式名称
             isDo:'false',//分组
             date:''
-        }
+        };
 
+        //添加todo任务
         $scope.addTodo = function(){
             $scope.todoList.push({
                 content:$scope.createTodo.content,//获取提交任务内容
@@ -38,16 +39,35 @@ angular
                 isDo:'false',//分组
                 date:getNowDate()
             });
-
             $scope.createTodo.content = '';//清空内容
+            $scope.save();
             console.log($scope.todoList);
 
         };
 
 
 
+
+        //删除
+        <!--ng-click="todoList.splice($index,1)"点击删除当前条目 , ($index,1)循环遍历的索引值-->
+
+        $scope.removeTask = function (todo) {
+            $scope.todoList.splice($scope.todoList.indexOf(todo), 1);
+            $scope.save();
+        };
+
+        //保存
+        $scope.save = function () {
+            localStorage.setItem('todoList', angular.toJson($scope.todoList));
+        }
+
+        //获取当前系统时间
+        function getNowDate(){
+            return new Date().toLocaleDateString()
+        }
+
+
+
     }]);
 
-function getNowDate(){//获取当前系统时间
-    return new Date().toLocaleDateString()
-}
+
