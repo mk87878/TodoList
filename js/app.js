b391d8a895824d3e66515fb73cddae8b8a35b3b3
mingwen <mk87878@163.com>
2015-12-16 16:28:43 +0800

    修改颜色选折select标签为ng-option

--- js/app.js
+++ js/app.js
@@ -2,30 +2,48 @@
  * Created by zhangmingwen on 15/12/8.
  */
 angular
-    .module('todoApp',[])//创建程序模块
-    .controller('toDoCtrl',['$scope',function($scope){
+    .module('todoApp',['ngStorage'])//创建程序模块
+    .controller('toDoCtrl',['$scope',function(
+        $scope
+        //$localStorage,
+        //$sessionStorage,
+    ){
+        $scope.todoList = [];//存储所以todo
 
-        $scope.todoList = [];
-        $scope.teamList = [
-            {team:0},
+
+
+        $scope.colorOptions = [//todo列表颜色
+            {name:'green',code:'list-group-item-success'},
+            {name:'red',code:'list-group-item-danger'},
+            {name:'blue',code:'list-group-item-info'},
+            {name:'yellow',code:'list-group-item-warning'}
         ];
+        /*
+        $scope.todoList.color = $scope.colorOptions[0].name;//初始化颜色
+        console.log($scope.todoList.color);
+        //用不起:原因未知
+        */
 
-        $scope.add = function(){
+        $scope.createTodo = {//初始化颜色
+            content:'',//任务内容
+            color:'list-group-item-info',//css样式名称
+            isDo:'false',//分组
+            date:''
+        }
+
+        $scope.addTodo = function(){
             $scope.todoList.push({
-                content:$scope.content,//获取提交任务内容
-                color:$scope.color,//获取提交的css样式名称
-                team:0,//分组
-                date:getNowDate(),
+                content:$scope.createTodo.content,//获取提交任务内容
+                color:$scope.createTodo.color,//获取提交的css样式名称
+                isDo:'false',//分组
+                date:getNowDate()
             });
-            $scope.content = '';//清空内容
+
+            $scope.createTodo.content = '';//清空内容
+            console.log($scope.todoList);
+
         };
 
-        $scope.addTeam = function(){
-            $scope.teamList.push({
-                team:$scope.team,//分组
-            });
-            $scope.content = '';//清空内容
-        };
 
 
     }]);
